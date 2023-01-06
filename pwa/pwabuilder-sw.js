bc2b303a0ba7f56172a12fd3b707292e1d500f08
ikram749 <ikram.ipman@gmail.com>
2023-01-06 16:27:57 +0800

    test

--- pwa/pwabuilder-sw.js
+++ pwa/pwabuilder-sw.js
@@ -147,7 +147,7 @@ self.addEventListener("fetch", (event) => {
 
 // Network is back up, we're being awaken, let's do the requests we were trying to do before if any.
 self.addEventListener("sync", (event) => {
-  alert('submitFormDataFromIndexedDB');
+  //alert('submitFormDataFromIndexedDB');
   if (event.tag === "form-submission") {
     event.waitUntil(submitFormDataFromIndexedDB());
   }
